f1389336ba49375f7236a73300f2a4401f45ca9d
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2023-02-20 12:55:51 +0000

    - initial version

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿namespace RefDepends;
+
+using System.Reflection;
+
+public static class Program
+{
+  public static void Main(string[] args)
+  {
+    var targetFolder = args[0];
+
+    // [dll] --> [where used]
+    var refDepsMap = new Dictionary<string, HashSet<string>>();
+
+    var assyPaths = Directory.EnumerateFiles(targetFolder, "*.dll", SearchOption.TopDirectoryOnly).Where(IsAssembly);
+    foreach (var assyPath in assyPaths)
+    {
+      var assy = Assembly.LoadFile(assyPath);
+      var assyName = assy.GetName();
+      var refs = assy.GetReferencedAssemblies();
+      foreach (var thisRef in refs)
+      {
+        var thisRefName = thisRef.ToString();
+        if (!refDepsMap.ContainsKey(thisRefName))
+        {
+          refDepsMap[thisRefName] = new();
+        }
+
+        refDepsMap[thisRefName].Add(assyName.ToString());
+      }
+    }
+
+    foreach (var kvp in refDepsMap.OrderBy(x => x.Key))
+    {
+      Console.WriteLine(kvp.Key);
+      foreach (var val in kvp.Value)
+      {
+        Console.WriteLine($"  {val}");
+      }
+    }
+  }
+
+  private static bool IsAssembly(string assyPath)
+  {
+    try
+    {
+      _ = AssemblyName.GetAssemblyName(assyPath);
+    }
+    catch
+    {
+      return false;
+    }
+
+    return true;
+  }
+}
